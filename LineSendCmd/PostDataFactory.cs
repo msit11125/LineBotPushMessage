06a4630dc2c940ade31d26d5c59f45e6c9f17a7a
unknown <msit11125@soft-world.com.tw>
2017-05-31 11:45:55 +0800

    First commit

---
+++ LineSendCmd/PostDataFactory.cs
@@ -0,0 +1,29 @@
+﻿using LineSendCmd.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineSendCmd
+{
+
+    /// <summary>
+    /// PostData 簡單工廠
+    /// </summary>
+    public class PostDataFactory
+    {
+
+        public static PostData CreateTextPostData(string to, string s)
+            => new PostData()
+            {
+                to = to,
+                messages = new[] {
+                    new Message() {
+                    type = "text",
+                    text = s
+                }
+                }
+            };
+    }
+}
---
+++ LineSendCmd/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LineSendCmd
+{
+    class Program
+    {
+
+        static readonly string _token = "TLw4WeVS1t3rXo2qwbT0rRAw8HONf2rA2bH+bsw0DuN7xvHWg6iJXShhX0ZF/gSxllvgKrFr1REIJ8WJqibV/MFhllh65E89l/9iuZp5bpY2vEplj6azg581Cy9q8oqAUaJ3BTE4a7RYi7dGUh8HmgdB04t89/1O/w1cDnyilFU=";
+
+        /// <summary>
+        /// 程式進入點
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var t = ExcuteChatAsync(args[0], args[1]);
+            t.Wait();
+        }
+
+
+        /// <summary>
+        /// 非同步執行傳送訊息
+        /// </summary>
+        /// <param name="to">Channel Access Token</param>
+        /// <param name="msg">傳遞的訊息</param>
+        /// <returns></returns>
+        static async Task ExcuteChatAsync(string to, string msg)
+        {
+            LineMessagingService _LineService = new LineMessagingService(_token);
+            var status = await _LineService.PushMessage(to, msg);
+        }
+
+
+    }
+
+
+}
